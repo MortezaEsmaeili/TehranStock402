c2315735280b14a7e3bb03dabc8e30fb58e00134
MortezaEsmaeili <Morteza.007@gmail.com>
2024-01-12 23:07:14 +0330

    Try to Add Telegram to Project
    First Step

---
+++ Telegram.Tester/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MSHB.TsetmcReader.Service.Helper;
+
+Console.WriteLine("Hello, World!");
+TelegramHelper telegramHelper = new TelegramHelper();
+
+await telegramHelper.sendMessage("سلام این اولین پیام بات است");
